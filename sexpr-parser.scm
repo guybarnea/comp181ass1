d56410eb70c2f1a282bf0d5f4a776da8f4a4b99b
Guyb <guybarnea22@gmail.com>
2017-10-28 18:06:30 +0300

    Add files via upload

--- sexpr-parser.scm
+++ sexpr-parser.scm
@@ -6,6 +6,14 @@
 ;     <ProperList> <ImproperList> <Vector> <Quoted> <QuasiQuoted> <Unquoted> 
 ;	  <UnquoteAndSpliced> <CBName>  <InfixExtension> 
 ;))
+(define list->number
+  (lambda (base)
+    (lambda (lst)
+      (string->number (list->string lst) base))
+  ))
+
+(define list->hex-number (list->number 16))
+(define list->base10-number (list->number 10))
 
 (define <Boolean> 
   (new
@@ -16,4 +24,36 @@
    (*pack (lambda (_) #f))
    (*disj 2)
 done))
-  
+
+(define <HexChar>
+  (new
+   (*parser (range #\0 #\9))
+   (*parser (range-ci #\a #\f))
+   (*disj 2)
+  done))
+
+
+(define <Natural>
+  (new
+   (*parser (range #\0 #\9))
+   *plus
+   (*pack list->base10-number)
+done))
+
+(define <Integer>
+  (new
+   (*parser (char #\+))
+   (*parser <Natural>)
+   (*caten 2)
+   (*pack-with (lambda (sign num) num))
+
+   (*parser (char #\-))
+   (*parser <Natural>)
+   (*caten 2)
+   (*pack-with (lambda (sign num) (- num)))
+
+   (*parser <Natural>)
+
+   (*disj 3)
+done))
+
